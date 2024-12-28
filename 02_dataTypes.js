035af0b65882f41dd20a871de05ea8f9198eff24
nirmalchaudhary <ankuchaudhary789@gmial.com>
2024-12-28 13:48:12 +0530

    changes

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //todo  treat this code as newer version of js
+
+console.log(9+10); 
+console.log("hello");
+
+let name="nirmal";
+let age=21;
+let isAdult=true;
+
+//! primitive data types
+//number => integer, float, double
+//string => " ", ' ', ` `
+//boolean => true, false
+//null => null
+//undefined => undefined
+//array => [value1, value2, value3]
+//function => function name(){}
+//symbol => Symbol()  //todo  unique value in js memory location we will use in react js 
+
+//! non-primitive data types
+//object => {key:value}
+
+console.log(typeof name);
+console.log(typeof age);
