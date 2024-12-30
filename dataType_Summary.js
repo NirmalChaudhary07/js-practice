78304a9e5bfd58dddefde5d973cf57a7b29045d8
nirmalchaudhary <ankuchaudhary789@gmial.com>
2024-12-30 11:03:24 +0530

    summary

---
+++ dataType_Summary.js
@@ -0,0 +1,7 @@
+// primitive data type
+//**  1. Number 2. String 3. Boolean 4. Undefined 5. Null 6. Symbol 7. BigInt
+
+// Non primitive data type (reference)
+
+//** 1. Object 2. Array 3. Function 4. Date 5. RegExp
+
