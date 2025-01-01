0d06b87f411816f79e36a4e2827d93d03137669b
nirmalchaudhary <ankuchaudhary789@gmial.com>
2025-01-01 11:04:31 +0530

    summary

--- dataType_Summary.js
+++ dataType_Summary.js
@@ -1,4 +1,4 @@
-// primitive data type  ( stack memonry )
+// primitive data type  ( stack memory )
 //**  1. Number 2. String 3. Boolean 4. Undefined 5. Null 6. Symbol 7. BigInt
 
 // Non primitive data type (reference)
