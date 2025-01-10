ed6baf1e8a79051cf5481a00b688c3b3b8d31ae1
nirmalchaudhary <ankuchaudhary789@gmial.com>
2025-01-10 12:51:03 +0530

    maths

---
+++ 04_nums_and_math.js
@@ -0,0 +1,13 @@
+const num =400;
+// console.log(num)
+const balance = new Number(100);
+// console.log(balance)
+
+// ++++++++++++++++++++ Math ++++++++++++++++++++++++++
+
+// console.log(Math.floor(Math.random()*10) +1)
+
+let min =10;
+let max =20;
+
+console.log(Math.floor(Math.random()*(max - min +1)+min))
