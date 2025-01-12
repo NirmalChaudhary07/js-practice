3f9e34daf275862026f783607c0272cc56a0614c
nirmalchaudhary <ankuchaudhary789@gmial.com>
2025-01-12 11:02:00 +0530

    changes

--- 02_array/02_array2.js
+++ 02_array/02_array2.js
@@ -20,6 +20,6 @@ let realArr =anotherArr.flat(Infinity)
 // console.log(realArr)
 
 console.log(Array.isArray("nirmal"));
-console.log(Array.from("nirmal"));
+console.log(Array.from("nirmal")); // convert the string into array 
 let score=100; let score2 =200; let score3 =300;
-console.log(Array.of(score,score2,score3))
+console.log(Array.of(score,score2,score3)) // convert the multiples variable into array 
