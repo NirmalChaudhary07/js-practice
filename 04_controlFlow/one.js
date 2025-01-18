ba59509392f5ad68d7ef8b32cc27c9993c91499f
nirmalchaudhary <ankuchaudhary789@gmial.com>
2025-01-18 21:42:10 +0530

    controlflow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,13 @@
+// if
+
+// const temp=41;
+// if(temp<=50){
+//     console.log("temp is less then 50");
+// }
+// else{
+//     console.log("greater then 50");
+// }
+
+
+const balance =1000;
+// if(balance>500) console.log("test"); //* implesite scope
